f81e5e7985c19dd6d05060c77ace688c0ffa4e8a
Warren Harper II <warrenharper2@att.net>
2019-09-04 22:10:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,16 +21,19 @@ end
       end
     end
 
-  def negative?
-    @sides.each do |length|
-      if(length <= 0)
-        return true
+    def validate_triangle
+        real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+        [a, b, c].each do |side|
+          real_triangle << false if side <= 0 
+        raise TriangleError if real_triangle.include?(false)
+        end
+        class TriangleError < StandardError   # the kind method should raise a custom error, TriangleError if the triangle is invalid
       end
-    end
+  
   end
 
 
 
-class TriangleError < StandardError   # the kind method should raise a custom error, TriangleError if the triangle is invalid
 
-end
+
+
