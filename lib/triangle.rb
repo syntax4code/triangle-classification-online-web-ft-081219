8192799d8839385dddd721185064f6652a695a3e
Warren Harper II <warrenharper2@att.net>
2019-09-04 22:10:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -24,16 +24,10 @@ end
     def validate_triangle
         real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
         [a, b, c].each do |side|
-          real_triangle << false if side <= 0 
+          real_triangle << false if side <= 0
         raise TriangleError if real_triangle.include?(false)
         end
         class TriangleError < StandardError   # the kind method should raise a custom error, TriangleError if the triangle is invalid
       end
-  
+
   end
-
-
-
-
-
-
