7f52bb5fef6350347bd233f344b5ed82c3981e54
Warren Harper II <warrenharper2@att.net>
2019-09-04 21:57:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,16 +1,16 @@
-class Triangle
+class Triangle  #Triangle class that accepts three arguments on initialization
 
 end
 
   attr_reader :side1, :side2, :side3
   @sides = []
 
-  def initialize(side1, side2, side3)
+  def initialize(side1, side2, side3) #the 3 arguments that are initialized
     @side1, @side2, @side3 = side1, side2, side3
     @sides = [side1, side2, side3]
   end
 
-  def kind
+  def kind  #Give your Triangles an instance method, kind that returns, as a symbol, :equilateral, :isosceles, :scalene
       validate_triangle
       if a == b && b == c
         :equilateral
